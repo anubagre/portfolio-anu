2d04f29476d2b5267b5c3c05e89567fb625a65c3
aaravgavshinde <aaravgavshinde@gmail.com>
2024-04-13 12:43:43 +0530

    added courses and achi

--- src/Components/Achievements.jsx
+++ src/Components/Achievements.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import '../Styles/achievements.css'
 
-export default function Achievements({componentRef}) {
+export default function Achievements({ componentRef }) {
   return (
     <div className='achievements-main-div'>
       <div className='empty-space' ref={componentRef}></div>
       <div className='achievements-heading'>Achievements</div>
+      <div className="all-achievements">
+        <div className="achi-slider">
+          <div><a href="https://www.linkedin.com/posts/anu-bagre_team-techfest-winner-activity-7027564170933854208-4E8K?utm_source=share&utm_medium=member_desktop" target='_blank'><b>1st rank</b> in Coding Competition, <b>Satyarth</b> (Tech fest 2023, SATI, Vidisha); Team size-5</a></div>
+          <div><a href="https://www.linkedin.com/posts/anu-bagre_kriyeta-hackathon-innovation-activity-7167070886401970176-n_3C?utm_source=share&utm_medium=member_desktop" target='_blank'>Amongst <b>Top 5</b> in Kriyeta 2.0 hackathon, Team size 5</a></div>
+          <div>Amongst <b>Top 10</b> in <b>SIH, 2023 internal hackathon</b>, Team size 6 </div>
+          <div><a href="https://youtu.be/5ggWsD5qUi4?si=DP0El7aH3idQomDx" target='_blank'>Secured <b>1st rank</b> in English Poetry Competition at Samrat Utsav, 2023</a></div>
+          <div>Secured <b>2nd rank</b> in Hindi Extempore Competition at Samrat Utsav, 2022</div>
+          <div><a href="https://drive.google.com/file/d/1ymF4oRjuW7_LSCM0RiqzrTbHDKpSe3Mc/view?usp=sharing" target='_blank'>Participated in <b>Google Solution Challenge</b>, 2024</a></div>
+          <div>Participated in Debate, Quiz, Slogan, Poem, Drawing, and Essay Writing Competition and received applauds</div>
+        </div>
+        <div className="achi-slider">
+          <div><a href="https://www.linkedin.com/posts/anu-bagre_team-techfest-winner-activity-7027564170933854208-4E8K?utm_source=share&utm_medium=member_desktop" target='_blank'><b>1st rank</b> in Coding Competition, <b>Satyarth</b> (Tech fest 2023, SATI, Vidisha); Team size-5</a></div>
+          <div><a href="https://www.linkedin.com/posts/anu-bagre_kriyeta-hackathon-innovation-activity-7167070886401970176-n_3C?utm_source=share&utm_medium=member_desktop" target='_blank'>Amongst <b>Top 5</b> in Kriyeta 2.0 hackathon, Team size 5</a></div>
+          <div>Amongst <b>Top 10</b> in <b>SIH, 2023 internal hackathon</b>, Team size 6 </div>
+          <div><a href="https://youtu.be/5ggWsD5qUi4?si=DP0El7aH3idQomDx" target='_blank'>Secured <b>1st rank</b> in English Poetry Competition at Samrat Utsav, 2023</a></div>
+          <div>Secured <b>2nd rank</b> in Hindi Extempore Competition at Samrat Utsav, 2022</div>
+          <div><a href="https://drive.google.com/file/d/1ymF4oRjuW7_LSCM0RiqzrTbHDKpSe3Mc/view?usp=sharing" target='_blank'>Participated in <b>Google Solution Challenge</b>, 2024</a></div>
+          <div>Participated in Debate, Quiz, Slogan, Poem, Drawing, and Essay Writing Competition and received applauds</div>
+        </div>
+      </div>
+      <div className='end-text'>Thanks for visiting!</div>
     </div>
   )
 }
--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
 import '../Styles/courses.css'
 
-export default function Courses({componentRef}) {
+export default function Courses({ componentRef }) {
   return (
     <div className='courses-main-div'>
       <div className='empty-space' ref={componentRef}></div>
       <div className='courses-heading'>Courses</div>
+      <div className="all-courses">
+        {/* AI Builder */}
+        <div className="ai-builder course">
+          <div className="course-heading">AI Builder</div>
+          <hr />
+          <div className='course-detail'>
+            <div className='course-by'>MPSSDEGB, FITT IITD</div>
+            <div className="course-date">January 2024 – March 2024</div>
+          </div>
+          <div className='course-description'>During this 80-hour online course, I explored diverse topics including machine learning, neural networks, and natural language processing.</div>
+          <div className='course-buttons'>
+            <a href="https://drive.google.com/file/d/1GVze5SKC8HJ1ZDYNKIFZ9Yv5b0YZx5oE/view?usp=sharing" target='_blank'>Certificate</a>
+            <a href="https://mp-iitd.in/index.php/cohort-1-january-to-march-2024-certificates/" target='_blank'>Verify</a>
+          </div>
+        </div>
+        {/* Python for DS, AI and Dev */}
+        <div className="python-1 course">
+          <div className="course-heading">Python for Data Science, AI, and Development</div>
+          <hr />
+          <div className='course-detail'>
+            <div className='course-by'>Coursera, IBM</div>
+            <div className="course-date">May 2023</div>
+          </div>
+          <div className='course-description'>I have completed this five-week course from Coursera with a score of <b>91.93%</b> </div>
+          <div className='course-buttons'>
+            <a href="https://drive.google.com/file/d/1KIaluy-YDMS7aMYrdf0y6U8QhZ0VPXFz/view?usp=sharing" target='_blank'>Certificate</a>
+            <a href="https://coursera.org/verify/G7SNTKHQDAKY" target='_blank'>Verify</a>
+          </div>
+        </div>
+        {/* Python for DS */}
+        <div className="python-2 course">
+          <div className="course-heading">Python for Data Science</div>
+          <hr />
+          <div className='course-detail'>
+            <div className='course-by'>IITM, NPTEL</div>
+            <div className="course-date">January 2023 - March 2023</div>
+          </div>
+          <div className='course-description'>This four-week course aimed at equipping participants to use Python programming for solving data science problems. I scored <b>76%</b> on the final exam and received an <b>Elite+Silver NPTEL Certificate</b>.</div>
+          <div className='course-buttons'>
+            <a href="https://drive.google.com/file/d/1uUidtbK1edavivXLcyLT_RioYJm5xzoE/view?usp=sharing" target='_blank'>Certificate</a>
+            <a href="https://nptel.ac.in/noc/E_Certificate/NPTEL23CS21S3428013503025410" target='_blank'>Verify</a>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
